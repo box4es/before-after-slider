fe7b2d2d3f9a39ba00f53525797f6e4d6147b8be
Евгений Соболев <evgeniy.sobolev@reg.ru>
2020-05-01 15:00:02 +0300

    mvp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <before-after-slider 
+      :before="require('@/assets/old.jpg')"
+      :after="require('@/assets/new.jpg')"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeforeAfterSlider from './components/BeforeAfterSlider.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BeforeAfterSlider,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
---
+++ src/components/BeforeAfterSlider.vue
@@ -0,0 +1,82 @@
+<template>
+  <div
+    :style="{
+      width: '1050px',
+      height: '650px',
+      background: getBackgroundUrl(after),
+    }"
+    class="bottom-layer"
+  >
+    <div
+      :style="{
+        width: sliderValue + '%',
+        height: '100%',
+        background: getBackgroundUrl(before),
+      }"
+      class="top-layer"
+    />
+    <range-slider
+      class="slider"
+      min="0"
+      max="100"
+      step="2"
+      v-model="sliderValue">
+    </range-slider>
+  </div>
+</template>
+
+<script>
+import RangeSlider from 'vue-range-slider'
+import 'vue-range-slider/dist/vue-range-slider.css'
+
+export default {
+
+  components: {
+    RangeSlider
+  },
+
+  props: {
+    before: {
+      type: String,
+      required: true,
+    },
+    after: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data () {
+    return {
+      sliderValue: 60
+    }
+  },
+
+  methods: {
+    getBackgroundUrl( src ) {
+      return `url('${src}')`;
+    },
+  },
+};
+</script>
+
+<style>
+.slider {
+  width: 100%;
+  padding: 0;
+  position: absolute;
+  bottom: calc(50% - 10px);
+}
+
+.slider .range-slider-rail, .slider .range-slider-fill {
+    opacity: 0;
+}
+
+.bottom-layer {
+  position: relative;
+}
+
+.top-layer {
+  border-right: 2px solid #ddd;
+}
+</style>
